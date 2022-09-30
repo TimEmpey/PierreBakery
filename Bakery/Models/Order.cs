36c64eff521554c0c983a79494488541c6ab5833
Tim Empey <timothyempey@gmail.com>
2022-09-30 11:26:38 -0700

    add arguments for other metrics in order

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -7,12 +7,18 @@ namespace Bakery.Models
   {
     private static List<Order> _instances = new List<Order> {};
     public string Item { get; set; }
+    public string Amount { get; set; }
+    public string Price { get; set; }
+    public string Date { get; set; }
     public int Id { get; }
 
 
     public Order(string orderItem)
     {
       Item = orderItem;
+      Amount = orderAmount;
+      Price = orderPrice;
+      Date = orderPrice;
       _instances.Add(this);
       Id = _instances.Count;
     }
